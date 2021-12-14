b2049a39f05cb6a2656e488c1846ac468dcc0a30
creamstew <umetsu@pin-japan.com>
2021-12-14 17:33:56 +0900

    OGP画像のスタイリング修正

--- lib/createOgImage.ts
+++ lib/createOgImage.ts
@@ -13,7 +13,7 @@ export const createOgImage = (baseImageUrl: string, title: string, author?: Auth
       )}&txt-pad=90&txt-color=00695C&txt-size=24&txt-align=right,bottom&mark64=${base64url(
         `${author.image.url}?w=60&h=60`
       )}&mark-x=870&mark-y=500&blend64=${base64url(
-        `https://assets.imgix.net/~text?txtsize=48&txt-color=009688&w=1120&txt-align=middle&txtfont=Hiragino%20Sans%20W6&txt-track=2&txt64=${base64url(
+        `https://assets.imgix.net/~text?txtsize=48&txt-color=009688&w=1040&txt-align=middle&txtfont=Hiragino%20Sans%20W6&txt-track=2&txt64=${base64url(
           title
         )}`
       )}&blend-mode=normal&blend-align=top,middle&blend-x=80&blend-y=180`
