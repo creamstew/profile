7c3c76231fb99e55a30ae0e86ad07cb2a3d44044
creamstew <r2011o4223mq@gmail.com>
2023-11-20 00:06:07 +0900

    remove setting for eslint prettier

--- next.config.js
+++ next.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
+module.exports = {};
