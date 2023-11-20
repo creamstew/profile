5728cacf4e520317b9fa98a73734f1438c0d5cee
creamstew <r2011o4223mq@gmail.com>
2023-11-20 22:57:18 +0900

    型修正

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -2,7 +2,9 @@ import { ContentResponse, ContentsResponse } from './api';
 import { AuthorResponse } from './author';
 import { ImageResponse } from './image';
 
-export type BlogsResponse = ContentsResponse<BlogResponse>;
+export type BlogsResponse = ContentsResponse<BlogResponse> & {
+  title: string;
+};
 
 export type BlogResponse = ContentResponse<{
   title: string;
