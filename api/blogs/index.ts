9d15fe71496e0161bcc0468f132c3df3bc3b50ca
creamstew <r2011o4223mq@gmail.com>
2023-11-20 14:34:03 +0900

    aspida関連のファイル削除

--- api/blogs/index.ts
+++
@@ -1,9 +0,0 @@
-import { ContentsQuery } from '../../types/api';
-import { BlogResponse } from '../../types/blog';
-
-export type Methods = {
-  get: {
-    query?: ContentsQuery;
-    resBody: BlogResponse;
-  };
-};
