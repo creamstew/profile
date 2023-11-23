2e242957049baf840a4a3cd4d2230cb60510ac2c
creamstew <r2011o4223mq@gmail.com>
2023-11-23 10:35:29 +0900

    twitterカードの情報を追加

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -49,6 +49,13 @@ export async function generateMetadata({
     },
     twitter: {
       card: 'summary_large_image',
+      title: blog.title,
+      description: 'Tech Blog by creamstew',
+      images: [
+        {
+          url: ogImageUrl,
+        },
+      ],
     },
   };
 }
